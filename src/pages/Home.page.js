20cdd24e4ba9ffdd12f378f79c2eba55f8a9af26
Iqbal Fasri <iqbalx64@gmail.com>
2019-03-28 09:33:47 +0700

    Refactor Content Component

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -1,18 +1,21 @@
 import React from "react";
-import{ Button, Icon } from 'antd'
+import { Button, Icon } from "antd";
 
 // Import Component
 import Header from "../components/Header";
 import Content from "../components/Content";
 import Footer from "../components/Footer";
 
-function Homepage({ dataIsNull, contents }) {
-  return (
-    <div className="app">
-      <Header />
-      <Content dataIsNull={dataIsNull} contents={contents} />
-      <Footer />
-    </div>
-  );
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <Content dataIsNull={this.props.dataIsNull} contents={this.props.contents} />
+        <Footer />
+      </div>
+    );
+  }
 }
-export default Homepage;
+
+export default HomePage;
